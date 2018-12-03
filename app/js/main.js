41c6d962cbc417c15b1a46d6dcf381b8b7be7190
Kaustubh Mishal <kaustubhm339@gmail.com>
2018-12-04 02:32:43 +0530

    Created site structure and updated content for home and about section

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,22 @@
 $(document).ready(function() {
-   
+    // Full page js
+    $('#fullpage').fullpage({
+        sectionsColor: ['#272727', '#282632', '#272727', '#282632', '#272727', '#282632'],
+        anchors: ['Home', 'About-Me', 'Skills', 'Work-History', 'Portfolio', 'Contact'],
+        autoScrolling: true,
+        scrollHorizontally: true,
+        navigation: true,
+        navigationTooltips: ['Home', 'About-Me', 'Skills', 'Work-History', 'Portfolio', 'Contact'],
+        css3: true,
+        scrollingSpeed: 700,
+        autoScrolling: true,
+        fitToSection: true,
+        loopBottom: true,
+        fadingEffect: true,
+    });
+    $.fn.fullpage.setAllowScrolling(true);
 
-})
+    // Write Cusotome Js from here ...
+    
+
+});
