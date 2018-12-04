4df7dfdb281860460c553557b291583b57c0b9a4
Kaustubh Mishal <kaustubhm339@gmail.com>
2018-12-04 21:29:37 +0530

    add navigation functionality

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,4 @@
+var $menuList = $('.menu-list> ul> li');
 $(document).ready(function() {
     // Full page js
     $('#fullpage').fullpage({
@@ -9,14 +10,44 @@ $(document).ready(function() {
         navigationTooltips: ['Home', 'About-Me', 'Skills', 'Work-History', 'Portfolio', 'Contact'],
         css3: true,
         scrollingSpeed: 700,
-        autoScrolling: true,
         fitToSection: true,
         loopBottom: true,
         fadingEffect: true,
+        scrollOverflow: true
     });
     $.fn.fullpage.setAllowScrolling(true);
 
-    // Write Cusotome Js from here ...
-    
+    // ===========Write Cusotome Js from here ... ===============
+    // Navbar functionality
+    // Select nav main element
+        $('#button-nav').on('click touchStart', animateHamburger);
+        // Menu List toggle popup
+        $menuList.each(function () {
+            $(this).on('click touchStart', function (e) {
+                // console.log(e.target);
+                $('#pop-nav').toggle('pop-active');
+                $('.hamburger').removeClass('is-active');
+            });
+        });
 
 });
+
+// Hamburger Animation
+function animateHamburger(){
+    // on click on that element a popup of  background color #272727, opacity 90% will open
+    // nav hamburger animation
+    // check if hamburger class has "is-active" class
+    if ($('.hamburger').is('.is-active')) {
+        // If consist of above class remove class
+        $('.hamburger').removeClass('is-active');
+    }
+    else {
+        //else remove class
+        $('.hamburger').addClass('is-active');
+    }
+    //Delay pop animation
+    setTimeout( function(){
+        $('#pop-nav').toggle('pop-active');  
+    }, 250);
+}
+
